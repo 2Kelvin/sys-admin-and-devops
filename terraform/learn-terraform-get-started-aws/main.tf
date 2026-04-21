b85d57238788ea1dbcb2fa8fa66e32d023549035
2Kelvin <rocketmancodes02@gmail.com>
2026-04-21 10:23:15 +0300

    format new terraform files

--- terraform/learn-terraform-get-started-aws/main.tf
+++ terraform/learn-terraform-get-started-aws/main.tf
@@ -13,22 +13,22 @@ data "aws_ami" "ubuntu" {
 
 # from registry: vpc/networking module
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "5.19.0"
-  name = "tf-example-vpc"
-  cidr = "10.0.0.0/16"
-  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets = ["10.0.101.0/24"]
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "5.19.0"
+  name                 = "tf-example-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.101.0/24"]
   enable_dns_hostnames = true
 }
 
 # creating an AWS EC2 instance
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ module.vpc.default_security_group_id ]
-  subnet_id = module.vpc.private_subnets[0]
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
   tags = {
     Name = var.instance_name # EC2 instance name
   }
